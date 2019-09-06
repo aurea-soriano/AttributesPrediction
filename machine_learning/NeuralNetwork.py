4a370472b8d7bf8cb9e9dfa6113c30bbf70e39ef
Aurea Soriano Vargas (Avi) <aurea.soriano@gmail.com>
2019-09-06 14:27:15 -0300

    structure

---
+++ machine_learning/NeuralNetwork.py
@@ -0,0 +1,20 @@
+'''
+Created on Sep 6, 2019
+
+@author: aurea
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+if __name__ == '__main__':
+    print "aurea";
+    pass
