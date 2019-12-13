def5d621cb17a3506255f3ae8989b3f575058467
Aurea Soriano-Vargas <aurea.soriano@gmail.com>
2019-12-13 12:36:17 -0300

    int actual dRMS

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ Created on Sep 7, 2019
 
 @author: aurea
 '''
+
+import sys
 from utils.FileReader import FileReader
 from machine_learning.LinearRegression import LinearRegression
 from machine_learning.MultilayerPerceptron import MultilayerPerceptron
@@ -15,6 +17,7 @@ import numpy as np
 from scipy.spatial.transform import rotation
 
 
+
 class Main(object):
     '''
     classdocs
@@ -25,8 +28,9 @@ class Main(object):
         '''
         Constructor
         '''
-        self.attributes_files = ["Porosity", "Porosity-Effective Ref", "NTG", "Sw_base", "Sg_Base", "dSg", "dSw", "avg_dRMS", "int_dRMS"];
-        
+        self.attributes_files = ["Porosity", "Porosity-Effective Ref", "NTG", "Sw_base",
+        "Sg_Base", "dSg", "dSw", "avg_dRMS", "int_dRMS", "int_dRMS_10m", "int_dRMS_20m"];
+
         self.training_matrix = []
         self.x = []
         self.y = []
@@ -39,6 +43,8 @@ class Main(object):
         self.dsw = []
         self.avg_dRMS = []
         self.int_dRMS = []
+        self.int_dRMS_10 = []
+        self.int_dRMS_20 = []
         self.xdRMS = []
         self.ydRMS = []
         self. attributes_list = []
@@ -46,79 +52,85 @@ class Main(object):
         self.attributes_names = []
         self.features_matrix_dsw = []
         self.features_matrix_dsg = []
-        
+
     def create_training_data(self):
-        
+
         # reading Porosity
         porosityMatrix = FileReader.read("data/"+self.attributes_files[0], 20);
         self.x = np.squeeze(np.asarray(porosityMatrix[:,0]));
         self.y = np.squeeze(np.asarray(porosityMatrix[:,1]));
         self.porosity = np.squeeze(np.asarray(porosityMatrix[:,2]));
-        
+
         # reading Porosity-effective
         porosityEffectiveMatrix = FileReader.read("data/"+self.attributes_files[1], 20);
         self.porosity_effective = np.squeeze(np.asarray(porosityEffectiveMatrix[:,2]));
-        
+
         # reading ntglinear_model,
         ntgMatrix = FileReader.read("data/"+self.attributes_files[2], 20);
         self.ntg = np.squeeze(np.asarray(ntgMatrix[:,2]));
-        
+
         # reading sw_base
         swBaseMatrix = FileReader.read("data/"+self.attributes_files[3], 20);
         self.sw_base = np.squeeze(np.asarray(swBaseMatrix[:,2]));
-        
+
         # reading sg_base
         sgBaseMatrix = FileReader.read("data/"+self.attributes_files[4], 20);
         self.sg_base = np.squeeze(np.asarray(sgBaseMatrix[:,2]));
-        
+
         # reading dsg
         dsgMatrix = FileReader.read("data/"+self.attributes_files[5], 20);
         self.dsg = np.squeeze(np.asarray(dsgMatrix[:,2]));
-        
-        # reading dsw 
+
+        # reading dsw
         dswMatrix = FileReader.read("data/"+self.attributes_files[6], 20);
         self.dsw = np.squeeze(np.asarray(dswMatrix[:,2]));
-        
-        
-        # reading dRMS 
+
+
+        # reading dRMS
         avg_dRMSMatrix = FileReader.read("data/"+self.attributes_files[7], 20);
         self.xdRMS = np.squeeze(np.asarray(avg_dRMSMatrix[0:len(self.dsw),0]));
         self.ydRMS = np.squeeze(np.asarray(avg_dRMSMatrix[0:len(self.dsw),1]));
         self.avg_dRMS= np.squeeze(np.asarray(avg_dRMSMatrix[0:len(self.dsw),2]));
-        
-        # reading dRMS 
+
+        # reading dRMS
         int_dRMSMatrix = FileReader.read("data/"+self.attributes_files[8], 20);
         self.xdRMS = np.squeeze(np.asarray(int_dRMSMatrix[0:len(self.dsw),0]));
         self.ydRMS = np.squeeze(np.asarray(int_dRMSMatrix[0:len(self.dsw),1]));
         self.int_dRMS= np.squeeze(np.asarray(int_dRMSMatrix[0:len(self.dsw),2]));
-       
-       
-        
-        
-    def machine_learning_analysis(self):  
-        
-        
+
+        # reading actual dRMS
+        int_dRMSMatrix_10 = FileReader.read("data/"+self.attributes_files[9], 20);
+        self.int_dRMS_10= np.squeeze(np.asarray(int_dRMSMatrix_10[0:len(self.dsw),2]));
+
+        # reading actual dRMS
+        int_dRMSMatrix_20 = FileReader.read("data/"+self.attributes_files[10], 20);
+        self.int_dRMS_20= np.squeeze(np.asarray(int_dRMSMatrix_20[0:len(self.dsw),2]));
+
+
+    def machine_learning_analysis(self):
+
+
         attribute_names_dsw = ["x","y", "int_dRMS"];
         attribute_names_dsg = ["x","y", "int_dRMS"];
-        attributes_list = [self.x, self.y, self.dsg, self.dsw, self.int_dRMS];
-        attributes_matrix = np.c_[self.x, self.y, self.dsg, self.dsw, self.int_dRMS];
+        attributes_list = [self.x, self.y, self.dsg, self.dsw, self.int_dRMS_10];
+        attributes_matrix = np.c_[self.x, self.y, self.dsg, self.dsw, self.int_dRMS_10];
         attributes_names = ["x","y","dSg", "dSw", "int_dRMS"];
-       
-        
-        
+
+
+
         #np.c_[x,y, porosity, porosity_effective, ntg, sw_base, sg_base, dsg, dsw];
-        self.features_matrix_dsw = np.c_[self.x, self.y,self.int_dRMS];
-        self.features_matrix_dsg = np.c_[self.x, self.y,self.int_dRMS];
-     
+        self.features_matrix_dsw = np.c_[self.x, self.y,self.int_dRMS_10];
+        self.features_matrix_dsg = np.c_[self.x, self.y,self.int_dRMS_10];
+
         CorrelationAnalysis(attributes_list, attributes_matrix, attributes_names);
-        
+
         linearRegression = LinearRegression(self.features_matrix_dsw, self.dsw, attribute_names_dsw, "Target: DSW");
         multilayerPerceptron = MultilayerPerceptron(self.features_matrix_dsw, self.dsw, attribute_names_dsw, "Target: DSW");
         svRegression = SVRegression(self.features_matrix_dsw, self.dsw, attribute_names_dsw, "Target: DSW");
         xgBoost = XGBoost(self.features_matrix_dsw, self.dsw, attribute_names_dsw, "Target: DSW");
-         
+
         randomForest = RandomForest(self.features_matrix_dsw, self.dsw, attribute_names_dsw, "Target: DSW");
-        
+
         linearRegression2 = LinearRegression(self.features_matrix_dsg, self.dsg, attribute_names_dsg, "Target: DSG");
         multilayerPerceptron2 = MultilayerPerceptron(self.features_matrix_dsg, self.dsg, attribute_names_dsg, "Target: DSG");
         svRegression2 = SVRegression(self.features_matrix_dsg, self.dsg, attribute_names_dsg, "Target: DSG");
@@ -129,5 +141,5 @@ if __name__ == '__main__':
     main = Main("");
     main.create_training_data();
     main.machine_learning_analysis();
-    
+
     pass
--- utils/Utils.py
+++ utils/Utils.py
@@ -4,7 +4,7 @@ Created on Sep 11, 2019
 @author: aurea
 '''
 
-import numpy as np 
+import numpy as np
 import scipy
 from scipy import misc
 
@@ -25,7 +25,7 @@ class Utils(object):
         count = 0;
         nonFlag = True; #True==4 False==3
         rows_vector = [];
-        for line in fileReader: 
+        for line in fileReader:
             if(count<20):
                 fileWriter.write(line);
             else:
@@ -67,8 +67,8 @@ class Utils(object):
                             if(i  != len(result_vector)-1):
                                 fileWriter.write(" ");
                         fileWriter.write("\n");
-                        
-            count+=1;        
+
+            count+=1;
         if(len(rows_vector) > 0):
             result_vector = np.average(rows_vector, axis=0);
             rows_vector = [];
@@ -77,22 +77,22 @@ class Utils(object):
                 if(i  != len(result_vector)-1):
                     fileWriter.write(" ");
             fileWriter.write("\n");
-            
+
         fileReader.close();
         fileWriter.close();
-        
+
     @staticmethod
     def interpolated_drms_resolution():
         '''
         Constructor
         '''
-        input_file = "../data/dRMS";
-        output_file = "../data/int_dRMS";
+        input_file = "../data/dRMS_20m";
+        output_file = "../data/int_dRMS_20m";
         fileReader = open(input_file, "r");
         fileWriter= open(output_file,"w+");
         rows_vector = [];
         count = 0;
-        for line in fileReader: 
+        for line in fileReader:
             if(count<20):
                 fileWriter.write(line);
             else:
@@ -100,30 +100,25 @@ class Utils(object):
                 vector = np.array(text);
                 vector = vector.astype(np.float);
                 rows_vector.append(vector)
-            count+=1;        
-       
+            count+=1;
+
         A = np.array(rows_vector)
         print(A.shape)
-        
+
         B = misc.imresize(A, [18455,5], interp='bilinear', mode='F')
-                    
+
         print(B.shape)
-        
+
         for i in range(0, B.shape[0]):
             for j in range(0, B.shape[1]):
                 fileWriter.write(str(B[i][j]));
                 if(j  != B.shape[1]-1):
                     fileWriter.write(" ");
             fileWriter.write("\n");
-        
+
         fileReader.close();
         fileWriter.close();
-        
+
 if __name__ == '__main__':
     #Utils.average_drms_resolution();
     Utils.interpolated_drms_resolution();
-    
-    
-              
-        
-        
